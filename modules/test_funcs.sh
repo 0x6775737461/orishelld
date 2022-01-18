6fdf645a46dc41e2f71f834b94df20c0868dcd50
0x6775737461 <fabiogustavo@tuta.io>
2022-01-18 19:33:08 -0400

    using only 80 spaces per line

--- modules/test_funcs.sh
+++ modules/test_funcs.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 modules=(os python ram hd kernelv pkgs locale)
+
 for module in ${modules[@]}
 do
    source modules/get_${module}.sh
@@ -7,8 +8,10 @@ done
 
 test_funcs(){
    if [ "$#" -eq 0 ]; then
-      if get_os && get_python && get_ram && get_hd && get_kernelv && get_pkgs && get_locale; then
-         return 0
+		if get_os && get_python && \
+			get_ram && get_hd && \
+			get_kernelv && get_pkgs && get_locale; then
+				return 0
       fi
    else
       echo "ERROR: arguments have not yet been implemented"
