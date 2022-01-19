afe56f1d14d31d011bc29bd7e2cd90e8b6622e48
0x6775737461 <fabiogustavo@tuta.io>
2022-01-19 18:06:26 -0400

    using 'grep' instead 'awk'

--- modules/get_hd.sh
+++ modules/get_hd.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 get_hd(){
-   hd="$(awk '{ print $2 }' <(df -h | sed '/\/$/!d; ; s/[[:alpha:]]//g'))"
-   hd="${hd} GiB"
+	# '/\/$/!d' - if the directory is mounted on '/' (root)
+
+	# '/[[:alpha:]]//g' - keep only the numbers
+
+	# ([[:digit:]])+[[:alpha:]]/\n&\n/ - find the first storage space and put new line around him
+
+	# ^([0-9]){1,}[[:alpha:]] - take the first numbers with one or more digits
+
+	hd="$(sed -E ' /\/$/!d; s/([[:digit:]])+[[:alpha:]]/\n&\n/' <(df -h) | \
+		grep -Eo -m 1 '^([0-9]){1,}[[:alpha:]]')"
 }
